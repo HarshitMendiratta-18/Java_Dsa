609e1ad9d79ad798b7db8d7188dea5a875df19a7
Harshit Mendiratta <harshitmendiratta41@gmail.com>
2026-06-17 14:57:58 +0530

    Initial commit

--- src/LoopQuestion.java
+++ src/LoopQuestion.java
@@ -1,14 +1,43 @@
+import java.util.Scanner;
+
 public class LoopQuestion {
     static void main() {
-        for(int i=0;i<=5;i++){
-            System.out.println("Hello!");
-        }
-        for(int i=1;i<=9;i++){
-            System.out.println(i);
+        //q1
+//        for(int i=1;i<=10;i++){
+//            System.out.println(i);
+//        }
+
+        //q2:
+//        int i=1;
+//        while(i<=10){
+//            System.out.println(i);
+//            i++;
+//        }
+
+        //q3:
+//        for(int i=2;i<=10;i++){
+//            System.out.println(i++);
+//        }
+
+        //q4:
+//        for(int i=1;i<=10;i++){
+//            System.out.println("7 * "+ i + " = " + 7*i);
+//        }
+
+        //q5:
+//        int score=3;
+//        do{
+//            System.out.println("Menu");
+//        }while(score>3);
+
+        //q6:
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int number=sc.nextInt();
+        int count=0;
+        for(int i=1;i<=number;i++){
+            System.out.println();
         }
 
-        for(int i=1;i<=10;i++){
-            System.out.println(i*i);
-        }
     }
 }
